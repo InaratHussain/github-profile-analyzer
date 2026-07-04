ab0d9d45fdaaa4eb38a36b67a92cd0db1cea579f
Inarat <hussaininarat03@gmail.com>
2026-07-04 19:22:27 +0530

    fixed db connection

--- config/db.js
+++ config/db.js
@@ -1,14 +1,13 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+const db = mysql.createConnection(process.env.DB_URL);
 
-db.connect((err)=>{
-    if(err) throw err;
+db.connect((err) => {
+    if (err) {
+        console.error("DB connection error:", err);
+        return;
+    }
     console.log("MySQL Connected");
 });
 
